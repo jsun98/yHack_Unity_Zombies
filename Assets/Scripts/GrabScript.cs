051b1fe6233c2cd7a3f509983ffc7edae12db7f2
Eddy Luo <edluo1@bu.edu>
2016-11-12 17:22:12 -0500

    Added code to grab and throw objects with hand
    To be used with microcontroller

---
+++ Assets/Scripts/GrabScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrabScript : MonoBehaviour {
+    bool attach = false;
+    public GameObject parent;
+    public GameObject weapon;
+	// Use this for initialization
+	void Start () {
+        attach = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 direction = parent.GetComponent<Rigidbody>().velocity;
+        //Debug.Log(direction);
+        if (Input.GetAxis("Fire2") == 1)
+        {
+            Debug.Log("attached");
+            attach = true;
+        } else
+        {
+            attach = false;
+            if (weapon != null)
+            {
+                weapon.GetComponent<Rigidbody>().isKinematic = false;
+                weapon.transform.parent = null; // disconnect weapon
+                weapon.GetComponent<Rigidbody>().AddForce(direction * 200);
+                weapon = null; // deset weapon
+            }
+        }
+        //Debug.Log(attach);
+	}
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Yes");
+        if (attach)
+        {
+            //Debug.Log("Trying to grab");
+            if (col.gameObject.tag == "Grabbable")
+            { // grab object
+                //Debug.Log("Object grabbed");
+                weapon = col.gameObject;
+                weapon.GetComponent<Rigidbody>().isKinematic = true;
+                weapon.transform.parent = gameObject.transform; 
+            }
+        }
+    }
+}
